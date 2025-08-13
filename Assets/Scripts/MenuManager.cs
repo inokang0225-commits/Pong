89fa8924937b367a0f398542fd75db2a8c460564
강동윤 <davidkang8111@gmail.com>
2025-08-13 16:38:42 +0900

    6666

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -54,19 +54,56 @@ public class MenuManager : MonoBehaviour
         if (NetworkManager.Singleton.ConnectedClientsList.Count < 2)
         {
             response.Approved = true;
+            // 플레이어 오브젝트는 직접 생성할 것
             response.CreatePlayerObject = false;
+        }
+
+        else 
+        { 
+            response.Approved = false;
+            response.Reason = "Max player in session is 2";
         }
     }
 
     // 호스트로 게임을 생성할 때 호출되는 메서드
     public void CreateGameAsHost()
     {
+        // 네트워크 매니저 가져오기
+        var networkManager = NetworkManager.Singleton;
+        // 네트워크 트랜스포트 설정 가져오기
+        var transport = (UnityTransport)networkManager.NetworkConfig.NetworkTransport;
+        // 사용할 포트 번호 지정
+        transport.ConnectionData.Port = DefaultPort; // 7777
 
+        // 호스트로 게임을 시작
+        if (networkManager.StartHost())
+        {
+            // 게임에 참가하는 모든 플레이어가 로비 씬을 로드하도록 설정
+            networkManager.SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+        }
+        else
+        {
+            // 호스트 시작에 실패한 경우
+            infoText.text = "Host failed to start";
+            Debug.LogError("Host failed to start");
+        }  
     }
 
     // 클라이언트로 게임에 참여할 때 호출되는 메서드
     public void JoinGameAsClient()
     {
+        var networkManager = NetworkManager.Singleton;
+        var transport = (UnityTransport)networkManager.NetworkConfig.NetworkTransport;
 
+        // 호스트 주소를 설정
+        transport.SetConnectionData(hostAddressInputField.text, DefaultPort);
+
+        // 클라이언트로 게임에 참여
+        if (!NetworkManager.Singleton.StartClient()) 
+        {
+            // 클라이언트 시작에 실패한 경우
+            infoText.text = "Client failed to start";
+            Debug.LogError("Client failed to start");
+        }
     }
 }
